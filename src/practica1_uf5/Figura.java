0595db246c4da315d237c50b6950f6c7b9680657
ramon <ramon@Ramon>
2022-04-30 14:31:23 +0200

    Act 1

---
+++ src/practica1_uf5/Figura.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practica1_uf5;
+
+/**
+ *
+ * @author ramon
+ */
+public class Figura {
+    public Figura (int numCostats, int ...longitud ){
+}
+public void CreacioFigura(){
+}
+}
