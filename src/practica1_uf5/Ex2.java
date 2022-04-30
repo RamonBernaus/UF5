efc003dbd344d9fb887e88a393ed04632ef10e91
ramon <ramon@Ramon>
2022-04-30 14:31:07 +0200

    Act 1

---
+++ src/practica1_uf5/Ex2.java
@@ -0,0 +1,16 @@
+/*2. Crear una classe de nom Figura on el constructor rebi un número sencer amb el nombre de
+costats de la figura i la longitud de cada costat. Com el nombre de valors dels costats és
+variable, utilitzeu el pas de paràmetres de mida indefinida. */
+package practica1_uf5;
+
+/**
+ *
+ * @author ramon
+ */
+public class Ex2 {
+    public static void main(String[] args) {
+        Figura f = new Figura(costats);
+        f.CreacioFigura();
+    }
+}
+
